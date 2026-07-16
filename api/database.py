a51e04c0ce96640feefb1a1a125397fc4ac539b6
Thomas Catt <armaghanahmed@gmail.com>
2026-07-16 23:47:59 +0500

    inprog: adding database

---
+++ api/database.py
@@ -0,0 +1,17 @@
+import motor.motor_asyncio
+import os
+
+# Usually you would load this from your .env file
+# e.g., MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = "mongodb://localhost:27017"
+
+# Create the async MongoDB client
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+
+# Select the database (it will be created automatically when you insert data)
+db = client.yaphub_db
+
+# Example collections (similar to SQL tables)
+users_collection = db.get_collection("users")
+posts_collection = db.get_collection("posts")
+comments_collection = db.get_collection("comments")
